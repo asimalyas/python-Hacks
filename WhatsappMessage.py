3491d3511a36514db26db1105dc021b8969d7006
Sharma9116 <s64901604@gmail.com>
2026-02-13 10:33:29 +0530

    Update WhatsappMessage.py

--- WhatsappMessage.py
+++ WhatsappMessage.py
@@ -1,7 +1,7 @@
 import pywhatkit as kit
 
 # Phone number with country code 
-phone_number = "+1234567890"  # Replace with the recipient's phone number
+phone_number = "+91 9257864310"  # Replace with the recipient's phone number
 message = "Hello! This message is sent using Python � By code_with_asim"
 
 # Send instantly (opens WhatsApp Web)
